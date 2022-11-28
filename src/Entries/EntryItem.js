f70cf0139a98bf001b161dcd29c641989ab7856f
KSurfM <karlomarkovic@hotmail.com>
2022-11-28 13:18:31 +0100

    delete entry branch start

--- src/Entries/EntryItem.js
+++ src/Entries/EntryItem.js
@@ -1,12 +1,17 @@
 import { useHistory } from "react-router-dom";
 import Card from "../ui/Card";
 import classes from "./EntryItem.module.css";
+import { useContext } from "react";
+import AuthContext from "../store/auth-context";
 
 const EntryItem = (props) => {
+  const authCtx = useContext(AuthContext);
   const history = useHistory();
   const showDetailsHandler = () => {
     history.push("/" + props.id);
   };
+
+  const deleteEntryHandler = () => {};
 
   return (
     <li className={classes.item}>
@@ -20,7 +25,11 @@ const EntryItem = (props) => {
           <h3>{props.location}</h3>
         </div>
         <div className={classes.actions}>
+          <button className={classes.button_hidden}>X</button>
+
           <button onClick={showDetailsHandler}>Show Details</button>
+
+          <button className={classes.button_cancel}>X</button>
         </div>
       </Card>
     </li>
